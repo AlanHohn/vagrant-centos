355e7e64f4cac2d5adac9f6757a444b85618c170
Alan Hohn <Alan.M.Hohn@lmco.com>
2014-07-29 17:32:45 -0400

    tailor vars, move to centos 7

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
-NAME=centos65-x86_64
+NAME=centos7-x86_64
 TYPE=RedHat_64
-INSTALLER="./isos/CentOS-6.5-x86_64-minimal.iso"
-GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.6.iso"
+INSTALLER="../isos/CentOS-7.0-1406-x86_64-DVD.iso"
+GUESTADDITIONS="../isos/VBoxGuestAdditions.iso"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
 NATNET=10.0.2.0/24
